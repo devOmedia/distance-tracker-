f029441f4121d5379ae152569addc90938c27ea0
SyedMahamudulHasan <syedhasan.cse@gmail.com>
2023-04-17 12:19:28 +0600

    init home screen class

---
+++ lib/view/screens/home_screens.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeScreens extends ConsumerStatefulWidget {
+  const HomeScreens({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreensState();
+}
+
+class _HomeScreensState extends ConsumerState<HomeScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
