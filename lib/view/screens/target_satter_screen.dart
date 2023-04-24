813dca69a691bc79370bafb1e2ba2f05731cf752
SyedMahamudulHasan <syedhasan.cse@gmail.com>
2023-04-24 17:32:09 +0600

    init firebase

--- lib/view/screens/target_satter_screen.dart
+++ lib/view/screens/target_satter_screen.dart
@@ -73,7 +73,7 @@ class _TargetSatterScreenState extends ConsumerState<TargetSatterScreen> {
                       ),
                     ],
                   ),
-                )
+                ),
               ],
             ),
           ),
